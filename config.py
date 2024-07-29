c465e59e6733068983ecdd1869d1eb53e4dbd6d1
Visionx-x <telegrambott19@gmail.com>
2024-07-29 22:30:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Visionx-x/Kd/blob/master/config.py",
+    "https://github.com/Visionx-x/Kd",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
